8b3a97ade398951e05bb856e1489fa19a8dc52c0
Tom Prince <tom.prince@hocat.ca>
2024-12-19 17:56:13 -0800

    dust piles

--- kubejs/startup_scripts/hardmode/material_registry/platline.js
+++ kubejs/startup_scripts/hardmode/material_registry/platline.js
@@ -10,7 +10,7 @@ GTCEuStartupEvents.registry('gtceu:material', event => {
     event.create('iridium_dioxide_residue')
         .dust()
         .color(0x17182e).iconSet('rough')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('iridium', '2x oxygen', 'rare_earth');
 
     event.create('chloroplatinic_acid')
@@ -52,51 +52,55 @@ GTCEuStartupEvents.registry('gtceu:material', event => {
     event.create('platinum_metallic')
         .dust()
         .color(0xfffbc5).iconSet('metallic')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('platinum', 'rare_earth')
 
     event.create('palladium_metallic')
         .dust()
         .color(0x808080).iconSet('metallic')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('palladium', 'rare_earth')
 
     event.create('ammonium_hexachloroplatinate')
         .dust()
         .color(0xfef0c2).iconSet('metallic')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('2x nitrogen', '8x hydrogen', 'platinum', '6x chlorine')
 
     event.create('potassium_bisulfate')
         .dust()
         .color(0xfdbd68)
+        .flags(GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('potassium', 'hydrogen', 'sulfur', '4x oxygen')
 
     event.create('potassium_pyrosulfate')
         .dust()
         .color(0xfbbb66)
+        .flags(GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('2x potassium', '2x sulfur', '7x oxygen')
 
     event.create('zinc_sulfate')
         .dust()
         .color(0x846649).iconSet('fine')
+        .flags(GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('zinc', 'sulfur', '4x oxygen')
 
     event.create('sodium_nitrate')
         .dust()
         .color(0x846684).iconSet('rough')
+        .flags(GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('sodium', 'nitrogen', '3x oxygen')
 
     event.create('rhodium_nitrate')
         .dust()
         .color(0x776649).iconSet('fine')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('rhodium', '3x nitrogen', '9x oxygen')
 
     event.create('sodium_ruthenate')
         .dust()
         .color(0x3a40cb).iconSet('shiny')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('2x sodium', 'ruthenium', '4x oxygen')
 
     event.create('sodium_peroxide')
@@ -107,49 +111,49 @@ GTCEuStartupEvents.registry('gtceu:material', event => {
     event.create('ammonium_hexachloroiridiate')
         .dust()
         .color(0x644629).iconSet('rough')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('2x nitrogen', '2x hydrogen', 'iridium', '6x chlorine');
 
     event.create('platinum_group_residue')
         .dust()
         .color(0x64632e).iconSet('rough')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('iridium', 'osmium', 'rhodium', 'ruthenium', 'rare_earth');
 
     event.create('crude_platinum_residue')
         .dust()
         .color(0xfffbc5).iconSet('dull')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('platinum_raw');
 
     event.create('crude_palladium_residue')
         .dust()
         .color(0x909090).iconSet('dull')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('palladium_raw');
 
     event.create('iridium_group_sludge')
         .dust()
         .color(0x644629).iconSet('dull')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('iridium', 'osmium', 'ruthenium', 'rare_earth');
 
     event.create('crude_rhodium_residue')
         .dust()
         .color(0x666666).iconSet('dull')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('2x rhodium', 'water');
 
     event.create('rhodium_salt')
         .dust()
         .color(0x848484).iconSet('shiny')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('2x salt', '2x rhodium', '6x chlorine');
 
     event.create('sodium_methoxide')
         .dust()
         .color(0xd0d0f0).iconSet('dull')
-        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.NO_GENERATE_DUST_PILES)
         .components('carbon', '3x hydrogen', 'oxygen', 'sodium');
 });
 
@@ -160,28 +164,4 @@ GTCEuStartupEvents.materialModification(() => {
     GTMaterials.get('ammonium_hexachloroplatinate').setFormula('(NH4)2PtCl6');
     GTMaterials.get('rhodium_nitrate').setFormula('Rh(NO3)3');
     GTMaterials.get('ammonium_hexachloroiridiate').setFormula('(NH4)2IrCl6');
-    GTMaterials.get('rhodium_salt').setFormula('(NaCl)2(RhCl3)2');
-    [
-        'iridium_dioxide_residue',
-        'platinum_metallic',
-        'palladium_metallic',
-        'ammonium_hexachloroplatinate',
-        'potassium_bisulfate',
-        'potassium_pyrosulfate',
-        'zinc_sulfate',
-        'sodium_nitrate',
-        'rhodium_nitrate',
-        'sodium_ruthenate',
-        'sodium_peroxide',
-        'platinum_group_residue',
-        'crude_platinum_residue',
-        'crude_palladium_residue',
-        'iridium_group_sludge',
-        'crude_rhodium_residue',
-        'rhodium_salt',
-        'sodium_methoxide',
-    ].map(mat => GTMaterials.get(mat)).forEach(mat => {
-        TagPrefix.dustTiny.setIgnored(mat);
-        TagPrefix.dustSmall.setIgnored(mat);
-    });
 })
